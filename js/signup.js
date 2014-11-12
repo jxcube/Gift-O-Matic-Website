f0b64e584fdcb294cd4589d56fb8b72c180d4b7b
gapinex@gmail.com <gapinex@gmail.com>
2014-11-12 16:01:21 +0700

    bugs fixed

---
+++ js/signup.js
@@ -0,0 +1,9 @@
+function signup(){
+	var url = "http://api.giftomaticapp.com/user";
+	var username = document.getElementById("username");
+	var email = document.getElementById("email");
+	var password1 = document.getElementById("password1");
+	var password2 = document.getElementById("password2");
+
+	
+}
